a0581346c9d18c1eb96277d0092a89cef808a56b
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2019-01-19 22:58:51 -0800

    Support SNLI training

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 
 from datasets import _rocstories
+from datasets import _snli
 
 def rocstories(data_dir, pred_path, log_path):
     preds = pd.read_csv(pred_path, delimiter='\t')['prediction'].values.tolist()
@@ -16,3 +17,13 @@ def rocstories(data_dir, pred_path, log_path):
     valid_accuracy = logs[best_validation_index]['va_acc']
     print('ROCStories Valid Accuracy: %.2f'%(valid_accuracy))
     print('ROCStories Test Accuracy:  %.2f'%(test_accuracy))
+
+def snli(data_dir, pred_path, log_path):
+    preds = pd.read_csv(pred_path, delimiter='\t')['prediction'].values.tolist()
+    _, _, _, labels = _snli(os.path.join(data_dir, 'snli_1.0_test.txt'))
+    test_accuracy = accuracy_score(labels, preds)*100.
+    logs = [json.loads(line) for line in open(log_path)][1:]
+    best_validation_index = np.argmax([log['va_acc'] for log in logs])
+    valid_accuracy = logs[best_validation_index]['va_acc']
+    print('SNLI Valid Accuracy: %.2f'%(valid_accuracy))
+    print('SNLI Test Accuracy:  %.2f'%(test_accuracy))
--- datasets.py
+++ datasets.py
@@ -8,6 +8,7 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
 seed = 3535999445
+SNLI_LABELS = {'neutral': 0, 'entailment': 1, 'contradiction': 2}
 
 def _rocstories(path):
     with open(path, encoding='utf_8') as f:
@@ -49,3 +50,33 @@ def rocstories(data_dir, n_train=1497, n_valid=374):
     trY = np.asarray(trY, dtype=np.int32)
     vaY = np.asarray(vaY, dtype=np.int32)
     return (trX1, trX2, trX3, trY), (vaX1, vaX2, vaX3, vaY), (teX1, teX2, teX3)
+
+def _snli(path):
+    sents1 = []
+    sents2 = []
+    labels = []
+    with open(path, encoding='utf_8') as f:
+        seen_header = False
+        for line in f:
+            if not seen_header:
+                seen_header = True
+                continue
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            fields = line.split('\t')
+            label = SNLI_LABELS[int(fields[0])]
+            s1 = fields[5]
+            s2 = fields[6]
+            sents1.append(s1)
+            sents2.append(s2)
+            labels.append(label)
+        return sents1, sents2, labels
+
+def snli(data_dir):
+    train_sents1, train_sents2, train_ys = _snli(os.path.join(data_dir, 'snli_1.0_train.txt'))
+    dev_sents1, dev_sents2, dev_ys = _snli(os.path.join(data_dir, 'snli_1.0_dev.txt'))
+    test_sents1, test_sents2, _ = _snli(os.path.join(data_dir, 'snli_1.0_test.txt'))
+    train_ys = np.asarray(train_ys, dtype=np.int32)
+    dev_ys = np.asarray(dev_ys, dtype=np.int32)
+    return (train_sents1, train_sents2, train_ys), (dev_sents1, dev_sents2, dev_ys), (test_sents1, test_sents2)
